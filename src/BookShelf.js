f5b6559b8efa6871f5f2c3ebe206c53f26f8fd07
dkrefta <diego.krefta@gmail.com>
2017-11-11 19:17:44 -0200

    start the part of app.js and going to another files
     (creating book, bookshelf and select) passing the divs for them

---
+++ src/BookShelf.js
@@ -0,0 +1,9 @@
+import Book from './Book'
+
+const Bookshelf = ({ }) => (
+  <div className="bookshelf">
+  <h2 className="bookshelft-title"> {title}</h2>
+  <div clasName="bookshelf-books">
+  <ol className="books-grid">
+
+)
